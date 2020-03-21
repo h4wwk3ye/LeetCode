4d9e6f3207352a68964bb6c648e16dc363975543
Farhan <farhantahir3@gmail.com>
2020-03-21 23:54:30 +0530

    init

---
+++ 83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ *
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+ *
+ * algorithms
+ * Easy (44.43%)
+ * Likes:    1211
+ * Dislikes: 95
+ * Total Accepted:    420K
+ * Total Submissions: 945.3K
+ * Testcase Example:  '[1,1,2]'
+ *
+ * Given a sorted linked list, delete all duplicates such that each element
+ * appear only once.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->1->2
+ * Output: 1->2
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 1->1->2->3->3
+ * Output: 1->2->3
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+    }
+};
+// @lc code=end
+
---
+++ 85.maximal-rectangle.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=85 lang=cpp
+ *
+ * [85] Maximal Rectangle
+ *
+ * https://leetcode.com/problems/maximal-rectangle/description/
+ *
+ * algorithms
+ * Hard (36.11%)
+ * Likes:    2189
+ * Dislikes: 61
+ * Total Accepted:    156.6K
+ * Total Submissions: 433.7K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle
+ * containing only 1's and return its area.
+ * 
+ * Example:
+ * 
+ * 
+ * Input:
+ * [
+ * ⁠ ["1","0","1","0","0"],
+ * ⁠ ["1","0","1","1","1"],
+ * ⁠ ["1","1","1","1","1"],
+ * ⁠ ["1","0","0","1","0"]
+ * ]
+ * Output: 6
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        
+    }
+};
+// @lc code=end
+
